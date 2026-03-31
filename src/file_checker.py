42a9a6f57312d45f4f6beffc6178a321011c73d7
Alex Foster <wookie471@outlook.com>
2026-03-31 14:35:30 +0100

    restructuring

--- src/file_checker.py
+++ src/file_checker.py
@@ -21,19 +21,18 @@ def check_files_exist(parent_path):
         elif os.path.isfile(file_path) and x.startswith("trades_") and (".csv" in file_path):
             trades = True
 
-    var = {
-        "pnl": pnl,
-        "positions": positions,
-        "trades": trades
-    }
-
-
     if not pnl:
         logger.warning("pnl file is missing")
     if not positions:
         logger.warning("positions file is missing")
     if not trades:
         logger.warning("trades file is missing")
+
+    var = {
+        "pnl": pnl,
+        "positions": positions,
+        "trades": trades
+    }
 
     return var
 
