79c21396086e4a9f4ee2355dadeb0e345469f2a1
Alex Foster <wookie471@outlook.com>
2026-03-31 16:21:22 +0100

    validate positions file

--- src/validator.py
+++ src/validator.py
@@ -57,3 +57,54 @@ def validate_pnl(file_path):
 
     logger.info("pnl file validation successful")
     return True
+
+def validate_positions(file_path):
+    try:
+        df = pd.read_csv(file_path)
+        df_copy = df
+        required_columns = ['symbol','quantity','avg_price','market_price','unrealized_pnl','currency']
+        for col in required_columns:
+            if col not in df.columns:
+                logger.error(f"Missing column: {col}")
+                return False
+    except Exception as e:
+        logger.error(f"Error validating positions file: {e}")
+        return False
+    df.dropna(inplace=True)
+
+    try:
+        symbol = df['symbol'].astype(str)
+
+        symbol = symbol.str.contains(r"\s")
+
+        if symbol.any():
+            df = df[~symbol]
+            logger.info("Dropped rows with whitespace in symbol column")
+    except Exception as e:
+        logger.error(f"Error validating symbol column: {e}")
+        return False
+
+    try:
+        df.loc[:, 'quantity'] = pd.to_numeric(df['quantity'], errors='coerce')
+        df.loc[:, 'avg_price'] = pd.to_numeric(df['avg_price'], errors='coerce')
+        df.loc[:, 'market_price'] = pd.to_numeric(df['market_price'], errors='coerce')
+        df.loc[:, 'unrealized_pnl'] = pd.to_numeric(df['unrealized_pnl'], errors='coerce')
+        df.dropna(inplace=True)
+        logger.info("Dropped all non numeric rows")
+    except Exception as e:
+        logger.error(f"Error converting numeric columns to numeric: {e}")
+        return False
+    
+    try:
+        valid_mask = df['currency'].apply(is_valid_currency)
+        df = df.loc[valid_mask].copy()
+                    
+    except Exception as e:
+        logger.error(f"Error validating currency codes: {e}")
+        return False
+
+
+    logger.info("positions file validation successful")
+    dropped_rows = df_copy[~df_copy.index.isin(df.index)]
+    logger.info(f"Dropped rows due to invalid data:\n{dropped_rows}")
+    return True
